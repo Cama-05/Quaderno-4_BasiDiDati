4253ae19ea7cb60040430418755bdecabe687dbc
Cama-05 <112072207+Cama-05@users.noreply.github.com>
2025-06-01 17:37:53 +0200

    Update 1_📚_Corsi.py

--- pages/1_📚_Corsi.py
+++ pages/1_📚_Corsi.py
@@ -20,7 +20,7 @@ if check_connection():
 
     col1, col2 = st.columns(2)
     with col1:
-        st.write(total_courses)
+        st.write(result_count)
         st.metric("Numero totale corsi", total_courses[0].get('count'))
     with col2:
         st.metric("Numero tipi di corsi", total_types)
